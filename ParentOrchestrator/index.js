e1576a59cc461e9989b6e28f75fec752166f46c2
Jon Hastings <jonathan.hastings@ensono.com>
2021-01-06 11:47:13 +0000

    initial commit

---
+++ ParentOrchestrator/index.js
@@ -0,0 +1,25 @@
+﻿const df = require("durable-functions");
+const moment = require("moment");
+
+module.exports = df.orchestrator(function*(context) {
+    let response = {}
+    const deadline = moment.utc(context.df.currentUtcDateTime).add(5, "s");
+    const timeoutTask = context.df.createTimer(deadline.toDate());
+
+    const activityTask = context.df.callSubOrchestrator("SubOrchestrator", "World");
+    
+    const winner = yield context.df.Task.any([activityTask, timeoutTask]);
+    if (winner === activityTask) {
+        // success case
+        timeoutTask.cancel();
+        response.success = true;
+        response.value = activityTask.result;
+    }
+    else
+    {
+        // timeout case
+        response.success = false;
+    }
+
+    return response;
+});
---
+++ SubOrchestrator/index.js
@@ -0,0 +1,15 @@
+﻿const df = require("durable-functions");
+const moment = require("moment");
+
+module.exports = df.orchestrator(function* (context) {
+  
+    const input = context.df.getInput();
+    const activityTask = context.df.callActivity("Hello", input);
+
+    const deadline = moment.utc(context.df.currentUtcDateTime).add(60, "s");
+    const timeoutTask = context.df.createTimer(deadline.toDate());
+
+    const winner = yield context.df.Task.any([activityTask, timeoutTask]);
+    return winner;
+
+});
